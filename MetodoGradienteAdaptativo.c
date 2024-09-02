2f053b50914d30fe09cb7159780b0cb36b25a25c
Bazch <mikael.vidal@gmail.com>
2024-09-02 15:59:40 -0300

    mudanças de notações

--- MetodoGradienteAdaptativo.c
+++ MetodoGradienteAdaptativo.c
@@ -3,7 +3,7 @@
 
 double *funcaoErroQuadratico(double *coeficientes, double *y, double *x, int qntPontos, double taxaDeAprendizado)
 {
-    double erro = pow(10, 5);
+    double erro = 1e5;
     double erroa = 0;
     double dA, dB;
     double r;
@@ -12,7 +12,7 @@ double *funcaoErroQuadratico(double *coeficientes, double *y, double *x, int qnt
     while (((erro - erroa) / erro) > 0.01)
     {
         erroa = erro;
-
+        erro = 0.0;
         for (int i = 0; i < qntPontos; i++)
         {
             double previsao = coeficientes[0] * x[i] + coeficientes[1];
@@ -25,13 +25,13 @@ double *funcaoErroQuadratico(double *coeficientes, double *y, double *x, int qnt
         for (int i = 0; i < qntPontos; i++)
         {
             double previsao = coeficientes[0] * x[i] + coeficientes[1];
-            dA += taxaDeAprendizado * ((y[i] - previsao) * (-x[i])) / (pow(10, -8) + r);
-            dB += taxaDeAprendizado * ((y[i] - previsao) * (- 1)) / (pow(10, -8) + r);
+            dA += taxaDeAprendizado * ((y[i] - previsao) * (-x[i])) / (1e-8 + r);
+            dB += taxaDeAprendizado * ((y[i] - previsao) * (- 1)) / (1e-8 + r);
         }
         coeficientes[0] -= dA;
         coeficientes[1] -= dB;
         count ++;
-        printf("\na = %f, b = %f, erro = %f\n", coeficientes[0], coeficientes[1], erro);
+        printf("\na = %.3f, b = %.3f, erro = %.3f\n", coeficientes[0], coeficientes[1], erro);
         
     }
 
