079a0ab199a39b8b37446f4d0333ba3e1a2c7cb7
Bazch <mikael.vidal@gmail.com>
2024-09-05 16:24:55 -0300

    feat: funções para calculo da complexidade logaritimica

--- MetodoGradienteAdaptativo.c
+++ MetodoGradienteAdaptativo.c
@@ -22,7 +22,7 @@ double funcq(double a, double b, double x, double c)
     return a * (pow(x, 2)) + b * x + c;
 }
 
-void *primeiroGrau(double a, double b, double *y, double *x, int qntPontos, double taxaDeAprendizado)
+void primeiroGrau(double a, double b, double *y, double *x, int qntPontos, double taxaDeAprendizado)
 {
     double erro = 1e5;
     double erroa = 0;
@@ -73,7 +73,7 @@ double DfuncCq(double a, double b, double x, double c)
     return 0;
 }
 
-void *segundoGrau(double a, double b, double c, double *y, double *x, int qntPontos, double taxaDeAprendizado)
+void segundoGrau(double a, double b, double c, double *y, double *x, int qntPontos, double taxaDeAprendizado)
 {
     double erro = 1e5;
     double erroa = 0;
@@ -106,5 +106,51 @@ void *segundoGrau(double a, double b, double c, double *y, double *x, int qntPon
         printf("\na = %.3f, b = %.3f, c = %.3f , erro = %.3f\n", a, b, c, erro);
     }
 
+    printf("\n%i", count);
+}
+
+double funclog(double a, double b, double x){
+    return a * x + b*log(x);
+}
+
+double DfunclogA(double a, double b, double x){
+    return -x;
+}
+
+double DfunclogB(double a, double b, double x){
+    return -log(x);
+}
+
+void logn(double a, double b, double *y, double *x, int qntPontos, double taxaDeAprendizado){
+    double erro = 1e5;
+    double erroa = 0;
+    double dA, dB, r;
+    int count = 0;
+    double theta = 1e-8;
+
+    while ((fabs(erro - erroa) / erro) > 0.01)
+    {
+        erroa = erro;
+        erro = 0;
+        for (int i = 0; i < qntPontos; i++)
+        {
+            erro += pow(y[i] - funclog(a, b, x[i]), 2);
+        }
+        erro /= qntPontos;
+        r = sqrt(erro);
+        dA = dB = 0.0;
+
+        for (int i = 0; i < qntPontos; i++)
+        {
+
+            dA += (taxaDeAprendizado * ((y[i] - funclog(a, b, x[i])) * DfunclogA(a, b, x[i]))) / (theta + r);
+            dB += (taxaDeAprendizado * ((y[i] - funclog(a, b, x[i])) * DfunclogB(a, b, x[i]))) / (theta + r);
+        }
+        a -= dA;
+        b -= dB;
+
+        count++;
+        printf("\na = %.3f, b = %.3f , erro = %.3f\n", a, b, erro);
+    }
     printf("\n%i", count);
 }
--- MetodoGradienteAdaptativo.h
+++ MetodoGradienteAdaptativo.h
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "MetodoGradienteAdaptativo.c"
-void *primeiroGrau(double a, double b, double *y, double *x, int qntPontos, double taxaDeAprendizado);
+void primeiroGrau(double a, double b, double *y, double *x, int qntPontos, double taxaDeAprendizado);
 double func(double a, double b, double x);
 double DfuncA(double a, double b, double x);
 double DfuncB(double a, double b, double x);
-void *segundoGrau(double a, double b, double c, double *y, double *x, int qntPontos, double taxaDeAprendizado);
+void segundoGrau(double a, double b, double c, double *y, double *x, int qntPontos, double taxaDeAprendizado);
 double funcq(double a, double b, double x, double c);
 double DfuncAq(double a, double b, double x, double c);
 double DfuncBq(double a, double b, double x, double c);
 double DfuncCq(double a, double b, double x, double c);
+void logn(double a, double b, double *y, double *x, int qntPontos, double taxaDeAprendizado);
+double DfunclogB(double a, double b, double x);
+double DfunclogA(double a, double b, double x);
+double funclog(double a, double b, double x);
